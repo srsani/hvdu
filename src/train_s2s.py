83176b88889bfe0e471671d5022d5e548bdf4ec6
srsani <srsani@gmail.com>
2023-10-06 18:00:11 +0200

    adding config code

--- src/train_s2s.py
+++ src/train_s2s.py
@@ -6,8 +6,35 @@ MIT License
 import argparse
 from sconf import Config
 
+from datasets import load_dataset
+import torch
+import time
+import evaluate
+import pandas as pd
+import numpy as np
+import ast
+
+
+from transformers import (AutoModelForSeq2SeqLM,
+                          AutoTokenizer,
+                          GenerationConfig,
+                          TrainingArguments,
+                          Trainer,
+                          EarlyStoppingCallback,
+                          IntervalStrategy)
+
+from peft import (LoraConfig,
+                  get_peft_model,
+                  TaskType,
+                  PeftModel,
+                  PeftConfig)
+
 
 def train(config):
+    df1 = pd.read_csv(config.df1_path)
+    print(config.df1_path)
+    df1.fillna('None', inplace=True)
+    print(df1.shape)
 
     return True
 
@@ -15,6 +42,7 @@ def train(config):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, required=True)
+
     args, left_argv = parser.parse_known_args()
 
     config = Config(args.config)
